688e8d372a8cb4ae43dd1fc03444c675efbde7fc
Max <sheterons@gmail.com>
2023-11-14 21:16:33 +0300

    improved regexp

--- src/components/DataField/DataField.tsx
+++ src/components/DataField/DataField.tsx
@@ -9,20 +9,22 @@ interface DataFieldProps {
 
 const regexPatterns = {
   person_full:
-    /(?:ФИО|Директор|Имя)[^\wА-ЯЁа-яё]*([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)/,
+    /(?:ФИО|Директор|Имя)[^\wА-ЯЁа-яё]*([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)/u,
   company_name_full:
-    /\b(ООО|Общество\s+с\s+ограниченной\s+ответственностью)[\s\S]*?«([^»]+)»/,
+    /\b(ООО|Общество\s+с\s+ограниченной\s+ответственностью)[\s\S]*?«([^»]+)»/u,
   inn: /\s?ИНН[\s\S]*?(\d{10}|\d{12})/,
   kpp: /\s?КПП[\s\S]*?(\d{9})/,
   combinedInnKpp: /\s?ИНН\/КПП[\s\S]*?(\d{10}|\d{12})\/(\d{9})/,
   ogrn: /\b(\d{13})\b/,
   bic: /\s?БИК[\s\S]*?(\d{9})\b/,
-  rs: /(?:Р\.С|Р\\С|Р\/С|Расс\. счёт|Расч[её]тный\s+сч[её]т)[\s\S]*?(\d{20})\b/,
-  ks: /(?:К\.С|К\\С|К\/С|Корр\. счёт|Корреспондентский\s+сч[её]т)[\s\S]*?(\d{20})\b/,
+  // eslint-disable-next-line max-len
+  rs: /(?:Р\.С|Р\\С|Р\/С|Расс\. счёт|Расч[её]тный\s+сч[её]т|Расч\/счет)[\s\S]*?(\d{4}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4})\b/iu,
+  // eslint-disable-next-line max-len
+  ks: /(?:К\.С|К\\С|К\/С|Корр\. счёт|Корреспондентский\s+сч[её]т|Кор\/сч)[\s\S]*?(\d{4}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4})\b/iu,
+
   email: /\b[Ee]-?[Mm]ail[\s\S]*?([\w.-]+@[\w.-]+\.\w+)\b/,
-  position:
-    /\b(директора?|генерального\sдиректора?|генеральный\sдиректор)[\s\S]*?\b/i,
-  person_action: /\b(устава?|устав)\b/i,
+  position: /(?<!\p{L})(директор|генеральный\s+директор)(?!\p{L})/iu,
+  person_action: /\b(устава?|устав)\b/iu,
 };
 
 export const DataField: FC<DataFieldProps> = ({ setParsedData }) => {
@@ -34,18 +36,19 @@ export const DataField: FC<DataFieldProps> = ({ setParsedData }) => {
   };
 
   const parseText = (text: string) => {
+    const cleanedText = text.replaceAll(/[\t\n\r]/g, ' ');
     const results: ParsedData = {};
 
-    const combinedMatch = text.match(regexPatterns.combinedInnKpp);
+    const combinedMatch = cleanedText.match(regexPatterns.combinedInnKpp);
     if (combinedMatch) {
       results['inn'] = combinedMatch[1];
       results['kpp'] = combinedMatch[2];
     } else {
-      const innMatch = text.match(regexPatterns.inn);
+      const innMatch = cleanedText.match(regexPatterns.inn);
       if (innMatch) {
         results['inn'] = innMatch[1];
       }
-      const kppMatch = text.match(regexPatterns.kpp);
+      const kppMatch = cleanedText.match(regexPatterns.kpp);
       if (kppMatch) {
         results['kpp'] = kppMatch[1];
       }
@@ -53,13 +56,19 @@ export const DataField: FC<DataFieldProps> = ({ setParsedData }) => {
 
     for (const [key, pattern] of Object.entries(regexPatterns)) {
       if (key !== 'inn' && key !== 'kpp' && key !== 'combinedInnKpp') {
-        const match = text.match(pattern);
+        const match = cleanedText.match(pattern);
         if (match) {
           if (key === 'person_full' && match[1] && match[2] && match[3]) {
             results[key as keyof ParsedData] =
               `${match[1]} ${match[2]} ${match[3]}`.trim();
           } else if (match[1]) {
-            results[key as keyof ParsedData] = match[1].trim();
+            if (key === 'rs' || key === 'ks') {
+              results[key as keyof ParsedData] = match[1]
+                .replace(/\s/g, '')
+                .trim();
+            } else {
+              results[key as keyof ParsedData] = match[1].trim();
+            }
           }
         }
       }
